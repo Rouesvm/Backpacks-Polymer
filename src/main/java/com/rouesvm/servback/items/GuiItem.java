faf4633436c8fae77a936500b72ce77e00a1e036
drivlo <rouesvmnowfree@gmail.com>
2024-10-11 11:51:08 +0800

    Fix bug

--- src/main/java/com/rouesvm/servback/items/GuiItem.java
+++ src/main/java/com/rouesvm/servback/items/GuiItem.java
@@ -3,10 +3,12 @@ package com.rouesvm.servback.items;
 import eu.pb4.sgui.api.gui.SimpleGui;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.Items;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.HitResult;
@@ -32,6 +34,17 @@ public class GuiItem extends BasicPolymerItem {
         createGui(serverPlayer, stack).open();
 
         return TypedActionResult.success(stack);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        if (!(context.getPlayer() instanceof ServerPlayerEntity serverPlayer))
+            return ActionResult.PASS;
+        if (serverPlayer.isSneaking())
+            return ActionResult.PASS;
+
+        createGui(serverPlayer, context.getStack()).open();
+        return ActionResult.success(true);
     }
 
     public SimpleGui createGui(ServerPlayerEntity player, ItemStack stack) {

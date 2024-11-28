46cae364893771fff3dd273f5468e79bc1b4d3b8
drivlo <rouesvmnowfree@gmail.com>
2024-11-28 23:38:08 +0800

    Gradle changes

--- src/main/java/com/rouesvm/servback/items/ContainerItem.java
+++ src/main/java/com/rouesvm/servback/items/ContainerItem.java
@@ -1,7 +1,9 @@
 package com.rouesvm.servback.items;
 
 import com.rouesvm.servback.Main;
+import com.rouesvm.servback.components.BackpacksDataComponentTypes;
 import com.rouesvm.servback.ui.BackpackGui;
+import net.fabricmc.fabric.api.item.v1.EnchantingContext;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ContainerComponent;
 import net.minecraft.enchantment.Enchantment;
@@ -20,6 +22,7 @@ import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 
 import java.util.List;
+import java.util.UUID;
 
 import static com.rouesvm.servback.Main.CAPACITY;
 
@@ -27,103 +30,52 @@ public class ContainerItem extends GuiItem {
     private final int slots;
     private int extendedSlots;
 
-    private final boolean enabled = false;
-
-    protected final int RADIUS = 5;
-
     public ContainerItem(String name, int slots) {
         super(name);
         this.slots = slots;
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if (!enabled) return;
-        if (world.isClient() || !entity.isPlayer()) return;
-        ServerPlayerEntity player = (ServerPlayerEntity) entity;
-
-        boolean nbt = stack.getOrDefault(Main.BOOLEAN_TYPE, false);
-        if (!nbt) {
-            Box area = new Box(player.getPos().add(-RADIUS, -RADIUS, -RADIUS), player.getPos().add(RADIUS, RADIUS, RADIUS));
-
-            List<ItemEntity> itemEntities = world.getEntitiesByType(EntityType.ITEM, area, Entity::isAlive);
-            SimpleInventory itemList = getInventory(stack);
-
-            for (ItemEntity item : itemEntities) {
-                item.setPosition(player.getX(), player.getY(), player.getZ());
-                if (item.cannotPickup())
-                    continue;
-
-                item.setPickupDelay(0);
-                item.kill();
-                itemList.addStack(item.getStack());
-            }
-
-            stack.set(DataComponentTypes.CONTAINER, ContainerComponent.fromStacks(itemList.getHeldStacks()));
-        }
-    }
-
-    @Override
-    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
-        ContainerComponent containerComponent = stack.getOrDefault(DataComponentTypes.CONTAINER, ContainerComponent.DEFAULT);
-
-        int capacityMaxShow = 0;
-        int capacityAmount = 0;
-
-        for (ItemStack itemStack : containerComponent.iterateNonEmpty()) {
-            capacityAmount++;
-
-            if (capacityMaxShow <= 4) {
-                capacityMaxShow++;
-                tooltip.add(Text.translatable("container.shulkerBox.itemCount", itemStack.getName(), itemStack.getCount()).formatted(Formatting.GOLD));
-            }
-        }
-
-        if (capacityAmount - capacityMaxShow > 0) {
-            tooltip.add(Text.translatable("container.shulkerBox.more", capacityAmount - capacityMaxShow).formatted(Formatting.ITALIC).formatted(Formatting.GOLD));
-        }
-    }
-
-    @Override
     public boolean canBeEnchantedWith(ItemStack stack, RegistryEntry<Enchantment> enchantment, EnchantingContext context) {
         if (slots == 9)
             return false;
-
         return super.canBeEnchantedWith(stack, enchantment, context);
     }
 
     @Override
     public void openGui(ServerPlayerEntity player, ItemStack stack) {
+        if (stack.get(BackpacksDataComponentTypes.UUID_TYPE) == null)
+            stack.set(BackpacksDataComponentTypes.UUID_TYPE, UUID.randomUUID().toString());
+
         onEnchanted(stack, player);
 
-        stack.set(Main.BOOLEAN_TYPE, false);
-        new BackpackGui(player, stack, getInventory(stack));
-    }
+        stack.set(BackpacksDataComponentTypes.BOOLEAN_TYPE, false);
+        new BackpackGui(player, stack, this.slots);    }
 
-    public SimpleInventory getInventory(ItemStack stack) {
-        return new SimpleInventory(getItemList(stack).toArray(ItemStack[]::new));
-    }
-
-    public DefaultedList<ItemStack> getItemList(ItemStack stack) {
-        DefaultedList<ItemStack> list = DefaultedList.ofSize(slots + extendedSlots, ItemStack.EMPTY);
+    public DefaultedList<ItemStack> getComponentItemList(ItemStack stack) {
+        DefaultedList<ItemStack> list = DefaultedList.ofSize(this.slots + this.extendedSlots, ItemStack.EMPTY);
         stack.getOrDefault(DataComponentTypes.CONTAINER, ContainerComponent.DEFAULT).copyTo(list);
         return list;
     }
 
-    public void onEnchanted(ItemStack stack, ServerPlayerEntity player) {
+    private DefaultedList<ItemStack> getItemList(ItemStack stack) {
+        UUID uuid = UUID.fromString(stack.get(BackpacksDataComponentTypes.UUID_TYPE));
+        return Main.backpackManager.getInventory(uuid, this.slots).getInventory();
+    }
+
+    private void onEnchanted(ItemStack stack, ServerPlayerEntity player) {
         DefaultedList<ItemStack> inventory = getItemList(stack);
 
         DynamicRegistryManager registryManager = player.getWorld().getRegistryManager();
-        RegistryEntry.Reference<Enchantment> capacity = registryManager.get(RegistryKeys.ENCHANTMENT).entryOf(CAPACITY);
+        RegistryEntry.Reference<Enchantment> capacity = registryManager.getOptional(RegistryKeys.ENCHANTMENT).get().getOrThrow(CAPACITY);
 
         int level = stack.getEnchantments().getLevel(capacity);
         extendedSlots = 9 * level;
 
         if (extendedSlots != 0) return;
-        if (inventory.size() < slots) return;
+        if (inventory.size() < this.slots) return;
 
-        for (int i = inventory.size(); i > slots; --i)
+        for (int i = inventory.size(); i > this.slots; --i)
             player.dropItem(inventory.get(i - 1), true);
-    }
-}
+    }}
 

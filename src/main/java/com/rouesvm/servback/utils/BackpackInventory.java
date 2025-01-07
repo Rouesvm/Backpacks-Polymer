281da0cccfe8ccce3af02f916d45564a6a74755c
drivlo <rouesvmnowfree@gmail.com>
2025-01-07 20:46:18 +0800

    Fix issue #13

--- src/main/java/com/rouesvm/servback/utils/BackpackInventory.java
+++ src/main/java/com/rouesvm/servback/utils/BackpackInventory.java
@@ -4,43 +4,26 @@ import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.collection.DefaultedList;
 
-public class BackpackInventory {
-    private DefaultedList<ItemStack> inventory;
-    private int size;
+public class BackpackInventory extends SimpleInventory {
 
     public BackpackInventory(int slots) {
-        this.inventory = DefaultedList.ofSize(slots, ItemStack.EMPTY);
-        this.size = slots;
+        super(slots);
     }
 
-    public BackpackInventory() {
-        this.inventory = DefaultedList.ofSize(9 * 6, ItemStack.EMPTY);
-        this.size = this.inventory.size();
-    }
+    public BackpackInventory(DefaultedList<ItemStack> stacks) {
+        super(stacks.size());
 
-    public int size() {
-        return this.size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public void setStack(int slot, ItemStack stack) {
-        this.inventory.set(slot, stack);
+        this.heldStacks.clear();
+        this.heldStacks.addAll(stacks);
     }
 
     public boolean insertItems(DefaultedList<ItemStack> itemStacks) {
         if (itemStacks != null && !itemStacks.isEmpty()) {
-            SimpleInventory itemList = getSimpleInventory();
-            itemStacks.forEach(itemList::addStack);
-
-            this.inventory = itemList.getHeldStacks();
+            markDirty();
+            itemStacks.forEach(this::addStack);
             return true;
         }
 
@@ -49,30 +32,25 @@ public class BackpackInventory {
 
     public void copyTo(BackpackInventory inventory) {
         for(int i = 0; i < inventory.size(); ++i) {
-            ItemStack itemStack = i < this.inventory.size() ? this.inventory.get(i) : ItemStack.EMPTY;
+            ItemStack itemStack = i < this.heldStacks.size() ? this.heldStacks.get(i) : ItemStack.EMPTY;
             inventory.setStack(i, itemStack.copy());
         }
     }
 
     public NbtCompound save(RegistryWrapper.WrapperLookup registryLookup) {
         NbtCompound nbtCompound = new NbtCompound();
-        return Inventories.writeNbt(nbtCompound, this.inventory, registryLookup);
+        return Inventories.writeNbt(nbtCompound, this.heldStacks, registryLookup);
     }
 
-    public void load(NbtCompound nbtCompound, RegistryWrapper.WrapperLookup registryLookup) {
-        Inventories.readNbt(nbtCompound, this.inventory, registryLookup);
-        this.size = this.inventory.size();
+    public static BackpackInventory load(NbtCompound nbtCompound, RegistryWrapper.WrapperLookup registryLookup) {
+        DefaultedList<ItemStack> itemStacks = DefaultedList.ofSize(9 * 6, ItemStack.EMPTY);
+        Inventories.readNbt(nbtCompound, itemStacks, registryLookup);
+
+        return new BackpackInventory(itemStacks);
     }
 
     public void setInventoryDirectly(DefaultedList<ItemStack> inventory) {
-        this.inventory = inventory;
-    }
-
-    public DefaultedList<ItemStack> getInventory() {
-        return this.inventory;
-    }
-
-    public SimpleInventory getSimpleInventory() {
-        return new SimpleInventory(this.inventory.toArray(ItemStack[]::new));
+        this.heldStacks.clear();
+        this.heldStacks.addAll(inventory);
     }
 }

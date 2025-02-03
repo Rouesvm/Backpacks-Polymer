103abd0a1b6df4beef8b5ab63b28c64540ded6cf
drivlo <rouesvmnowfree@gmail.com>
2025-02-03 17:06:33 +0800

    Data gen recipes.

---
+++ src/main/java/com/rouesvm/servback/datagen/BackpacksDataGenerator.java
@@ -0,0 +1,12 @@
+package com.rouesvm.servback.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BackpacksDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+        pack.addProvider(ModRecipeProvider::new);
+    }
+}

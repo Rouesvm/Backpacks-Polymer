5c7fe0b9c8a2de26abbdde3248e08e68f0c965c5
drivlo <rouesvmnowfree@gmail.com>
2025-02-05 17:15:10 +0800

    Pull commits from 1.21.4 branch.

--- src/main/java/com/rouesvm/servback/datagen/ModRecipeProvider.java
+++ src/main/java/com/rouesvm/servback/datagen/ModRecipeProvider.java
@@ -28,7 +28,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     private void itemRecipes(RecipeExporter exporter) {
-        for (int i = 1; i < 3; i++) {
+        for (int i = 1; i <= 3; i++) {
             ContainerItem backpack = (ContainerItem) ContainerItem.getDefaultBackpack(i);
             String backpackName = backpack.getIdentifier().getPath();
 
@@ -41,6 +41,8 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 Item dyeColor = DyeItem.byColor(color);
 
                 createTransmuteRecipe(exporter, backpack, dyeColor, ContainerItem.getColoredBackpack(color, i), name);
+
+                if (i+1 == 4) continue;
                 createUpgradeRecipe(exporter, ContainerItem.getColoredBackpack(color, i), backpackUpATier, slots, name, tierUpBackpackName);
             }
         }

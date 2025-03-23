d74b8ba19b25455745786286bebb519c98d85232
drivlo <rouesvmnowfree@gmail.com>
2025-03-23 22:33:58 +0800

    Successfully convert old data to new data.

--- src/main/java/com/rouesvm/servback/state/BackpackDataFixer.java
+++ src/main/java/com/rouesvm/servback/state/BackpackDataFixer.java
@@ -1,20 +1,27 @@
 package com.rouesvm.servback.state;
 
-import com.rouesvm.servback.state.codecs.SlotData;
+import com.rouesvm.servback.utils.BackpackInstance;
+import com.rouesvm.servback.utils.BackpackInventory;
+import com.rouesvm.servback.utils.BackpackManager;
+import net.minecraft.inventory.Inventories;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtIo;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.Uuids;
 import net.minecraft.util.WorldSavePath;
+import net.minecraft.util.collection.DefaultedList;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 public class BackpackDataFixer {
@@ -25,31 +32,47 @@ public class BackpackDataFixer {
         try (DataInputStream dataInputStream = new DataInputStream(
                 new GZIPInputStream(new FileInputStream(path.toFile())))) {
             oldData = NbtIo.readCompound(dataInputStream);
-        } catch (Exception e) {
-            System.out.println("Failed to read with GZIPInputStream: " + e.getMessage());
-        }
+        } catch (Exception ignored) {}
 
         if (oldData != null) {
-            System.out.println("Successfully read backpack data: " + oldData);
-            System.out.println(convertToV2Format(oldData, server.getRegistryManager()));
+            Set<BackpackInstance> instances = convertToV2Format(oldData, server.getRegistryManager());
+            instances.forEach(backpackInstance ->
+                    BackpackManager.getManager().storedInstances.put(backpackInstance.getUuid(), backpackInstance));
+
+            BackpackManager.getManager().save(server);
+
+            try {
+                Files.delete(path);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    private static List<SlotData> convertToV2Format(NbtCompound backpackData, RegistryWrapper.WrapperLookup registryLookup) {
-        List<SlotData> slotDataList = new ArrayList<>();
+    public static BackpackInstance load(NbtCompound compound, RegistryWrapper.WrapperLookup registryLookup) {
+        DefaultedList<ItemStack> stacks = DefaultedList.of();
+        Inventories.readNbt(compound.getCompound("contents").get(), stacks, registryLookup);
 
-        if (backpackData.contains("Items")) {
-            NbtList itemsList = backpackData.getList("Items").get();
+        BackpackInstance backpackInstance = new BackpackInstance(
+                Uuids.toUuid(compound.getIntArray("uuid").get()),
+                new BackpackInventory(stacks)
+        );
+        backpackInstance.lastAccessed = compound.getLong("lastAccessed").get();
 
-            for (int i = 0; i < itemsList.size(); i++) {
-                Optional<NbtCompound> itemNbt = itemsList.getCompound(i);
-                byte slot = itemNbt.get().getByte("Slot", (byte) 0);
+        return backpackInstance;
+    }
 
-                Optional<ItemStack> stack = ItemStack.fromNbt(registryLookup, itemNbt.get());
-                slotDataList.add(new SlotData((int) slot, stack.get()));
-            }
+    public static Set<BackpackInstance> convertToV2Format(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        Set<BackpackInstance> instances = new HashSet<>();
+
+        var data = nbt.get("data");
+        if (data instanceof NbtCompound compound) {
+            Optional<NbtList> list = compound.getList("backpackContents");
+
+            list.ifPresent(nbtElements -> nbtElements.forEach(element ->
+                    instances.add(load((NbtCompound) element, registryLookup))));
         }
 
-        return slotDataList;
+        return instances;
     }
 }

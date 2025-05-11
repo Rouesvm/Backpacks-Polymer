00aa84228d499e34c60b82342f154e22252193e2
drivlo <rouesvmnowfree@gmail.com>
2025-05-11 10:49:55 +0800

    This looks insane.

--- src/main/java/com/rouesvm/servback/registry/BackpackItemGroup.java
+++ src/main/java/com/rouesvm/servback/registry/BackpackItemGroup.java
@@ -5,13 +5,23 @@ import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils;
 import net.minecraft.item.ItemGroup.Entries;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 
 public class BackpackItemGroup {
     public static void addItems(Entries entries) {
-        BackpackItemRegistry.SMALL.values().forEach(entries::add);
-        BackpackItemRegistry.MEDIUM.values().forEach(entries::add);
-        BackpackItemRegistry.LARGE.values().forEach(entries::add);
+        BackpackItemRegistry.SMALL.forEach((color, item) -> {
+            if (color == DyeColor.LIGHT_GRAY) return;
+            entries.add(item);
+        });
+        BackpackItemRegistry.MEDIUM.forEach((color, item) -> {
+            if (color == DyeColor.LIGHT_GRAY) return;
+            entries.add(item);
+        });
+        BackpackItemRegistry.LARGE.forEach((color, item) -> {
+            if (color == DyeColor.LIGHT_GRAY) return;
+            entries.add(item);
+        });
 
         entries.add(BackpackItemRegistry.GLOBAL_BACKPACK);
         entries.add(BackpackItemRegistry.ENDER_BACKPACK);

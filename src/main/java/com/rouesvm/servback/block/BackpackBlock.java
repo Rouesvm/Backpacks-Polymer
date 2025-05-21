9ab486f4d2edc5857fbc5684990cdca3b2ed03de
drivlo <rouesvmnowfree@gmail.com>
2025-05-22 00:08:35 +0800

    Breaks with flow of water.

--- src/main/java/com/rouesvm/servback/block/BackpackBlock.java
+++ src/main/java/com/rouesvm/servback/block/BackpackBlock.java
@@ -21,13 +21,15 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
@@ -38,12 +40,26 @@ public class BackpackBlock extends BasicPolymerBlock implements BlockEntityProvi
     public BackpackBlock() {
         super(Settings.create()
                 .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(Main.MOD_ID, "backpack")))
-                .pistonBehavior(PistonBehavior.DESTROY)
-                .sounds(BlockSoundGroup.WOOL)
+                .noCollision()
                 .breakInstantly()
-                .nonOpaque()
-                .solidBlock(Blocks::never)
+                .pistonBehavior(PistonBehavior.DESTROY)
         );
+    }
+
+    @Override
+    protected BlockState getStateForNeighborUpdate(
+            BlockState state,
+            WorldView world,
+            ScheduledTickView tickView,
+            BlockPos pos,
+            Direction direction,
+            BlockPos neighborPos,
+            BlockState neighborState,
+            Random random
+    ) {
+        return world.getFluidState(neighborPos) != null
+                ? Blocks.AIR.getDefaultState()
+                : super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
     }
 
     @Override

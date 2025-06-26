818a240421ad46a6e74d29e672c451377d1bc677
drivlo <rouesvmnowfree@gmail.com>
2025-06-26 11:50:07 +0800

    Fix data loading

--- src/main/java/com/rouesvm/servback/technical/data/state/BackpackDataFixer.java
+++ src/main/java/com/rouesvm/servback/technical/data/state/BackpackDataFixer.java
@@ -41,8 +41,6 @@ public class BackpackDataFixer {
             instances.forEach(backpackInstance ->
                     BackpackManager.instance.storedInstances.put(backpackInstance.getUuid(), backpackInstance));
 
-            BackpackManager.save(server);
-
             try {
                 Files.delete(path);
             } catch (IOException e) {

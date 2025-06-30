edf832f0441fc9876b39ab809bff6a8863b8a635
drivlo <rouesvmnowfree@gmail.com>
2025-06-30 21:54:13 +0800

    Create backup on close.

--- src/main/java/com/rouesvm/servback/technical/config/commands/BackpackCommands.java
+++ src/main/java/com/rouesvm/servback/technical/config/commands/BackpackCommands.java
@@ -6,6 +6,7 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.rouesvm.servback.ServerBackpacks;
 import com.rouesvm.servback.technical.config.Configuration;
+import com.rouesvm.servback.technical.data.BackpackDataSaver;
 import com.rouesvm.servback.technical.data.BackpackInstance;
 import com.rouesvm.servback.technical.data.BackpackManager;
 import com.rouesvm.servback.technical.ui.BackpackGui;
@@ -31,6 +32,10 @@ public class BackpackCommands {
         dispatcher.register(literal("backpacks")
                 .executes(context -> {
                     context.getSource().sendFeedback(() -> Text.literal("Server Backpacks! by Rouesvm"), false);
+                    return 1;
+                }).then(literal("backup").executes(context -> {
+                    context.getSource().sendFeedback(() -> Text.translatable("command.serverbackpacks.backup"), false);
+                    BackpackDataSaver.createBackup();
                     return 1;
                 }).then(literal("list").executes(context -> {
                     Set<UUID> instances = BackpackManager.instance.storedInstances.keySet();
@@ -61,7 +66,7 @@ public class BackpackCommands {
                                 Text.translatable("command.serverbackpacks.empty"));
                     return 1;
                 }))).then(configCommand())
-        );
+        ));
     }
 
     private static LiteralArgumentBuilder<ServerCommandSource> configCommand() {

492e1839e8588a32af3fdc31b70803776e6332b2
drivlo <rouesvmnowfree@gmail.com>
2025-07-03 10:51:26 +0800

    Removed some unnecessary logging again.

--- src/main/java/com/rouesvm/servback/technical/data/BackpackDataSaver.java
+++ src/main/java/com/rouesvm/servback/technical/data/BackpackDataSaver.java
@@ -33,11 +33,7 @@ public class BackpackDataSaver {
     public static boolean onServerStarting(MinecraftServer server) {
         savePath = server.getSavePath(WorldSavePath.ROOT).resolve("data/serverbackpacks.data");
 
-        System.out.println(savePath);
-
         if (Files.exists(savePath)) {
-            ServerBackpacks.LOGGER.info("Loading...");
-
             try {
                 var data = SAVE_CODEC.decode(server.getRegistryManager().getOps(NbtOps.INSTANCE), NbtIo.readCompound(new DataInputStream(
                         new FileInputStream(savePath.toFile()))));

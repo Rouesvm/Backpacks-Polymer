51dee09bb9d148430528aa044f9ef08e2b63c7c7
drivlo <rouesvmnowfree@gmail.com>
2025-07-08 23:08:17 +0800

    Return isEmpty check

--- src/main/java/com/rouesvm/servback/content/upgrade/impl/MagnetUpgrade.java
+++ src/main/java/com/rouesvm/servback/content/upgrade/impl/MagnetUpgrade.java
@@ -57,7 +57,6 @@ public class MagnetUpgrade extends Upgrade {
     @Override
     public void tick(ServerPlayerEntity player, BackpackInventory inventory) {
         if (inventory == null) return;
-        if (inventory.isEmpty()) return;
 
         ServerWorld world = player.getWorld();
 

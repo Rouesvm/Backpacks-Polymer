449e826abc7cb7912a2b55affb1d724e4663b414
drivlo <rouesvmnowfree@gmail.com>
2025-07-09 23:31:46 +0800

    Compare using type

--- src/main/java/com/rouesvm/servback/content/upgrade/Upgrade.java
+++ src/main/java/com/rouesvm/servback/content/upgrade/Upgrade.java
@@ -1,5 +1,7 @@
 package com.rouesvm.servback.content.upgrade;
 
+import java.util.Objects;
+
 public class Upgrade implements FunctionalUpgrade {
     private final UpgradeType<? extends Upgrade> type;
 
@@ -9,5 +11,18 @@ public class Upgrade implements FunctionalUpgrade {
 
     public UpgradeType<? extends Upgrade> getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Upgrade upgrade = (Upgrade) o;
+        return Objects.equals(type, upgrade.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(type);
     }
 }

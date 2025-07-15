52c991ae0f9929c3f0901ccf7c7d6349980cb9a5
drivlo <rouesvmnowfree@gmail.com>
2025-07-15 22:54:28 +0800

    Renamed SaveableUpgrade.java to PersistentUpgrade.java.

--- src/main/java/com/rouesvm/servback/content/upgrade/PersistentUpgrade.java
+++ src/main/java/com/rouesvm/servback/content/upgrade/PersistentUpgrade.java
@@ -3,7 +3,7 @@ package com.rouesvm.servback.content.upgrade;
 import net.minecraft.storage.ReadView;
 import net.minecraft.storage.WriteView;
 
-public interface SaveableUpgrade {
-    default void readView(ReadView data) {}
-    default void writeView(WriteView data) {}
+public interface PersistentUpgrade {
+    void readView(ReadView data);
+    void writeView(WriteView data);
 }

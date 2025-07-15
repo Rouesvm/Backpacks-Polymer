2379e5e45e5b198005bb88cbf44aee2e57e6dd32
drivlo <rouesvmnowfree@gmail.com>
2025-07-15 22:54:15 +0800

    Renamed and clean up code.

---
+++ src/main/java/com/rouesvm/servback/content/recipe/InputDefinedFilterRecipe.java
@@ -0,0 +1,189 @@
+package com.rouesvm.servback.content.recipe;
+
+import com.rouesvm.servback.content.component.UpgradeComponent;
+import com.rouesvm.servback.content.item.UpgradeItem;
+import com.rouesvm.servback.content.upgrade.FilterableUpgrade;
+import com.rouesvm.servback.content.upgrade.Upgrade;
+import com.rouesvm.servback.content.upgrade.extension.ItemFilter;
+import com.rouesvm.servback.content.upgrade.impl.MagnetUpgrade;
+import com.rouesvm.servback.registry.BackpackDataComponentTypes;
+import com.rouesvm.servback.registry.BackpackRecipeRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.SpecialCraftingRecipe;
+import net.minecraft.recipe.book.CraftingRecipeCategory;
+import net.minecraft.recipe.input.CraftingRecipeInput;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InputDefinedFilterRecipe extends SpecialCraftingRecipe {
+    public static final RecipeSerializer<InputDefinedFilterRecipe> SERIALIZER = new InputDefinedFilterRecipe.Serializer(InputDefinedFilterRecipe::new);
+
+    public InputDefinedFilterRecipe(CraftingRecipeCategory category) {
+        super(category);
+    }
+
+    @Override
+    public boolean matches(CraftingRecipeInput input, World world) {
+        if (input.isEmpty()) return false;
+        if (input.getStackCount() > MagnetUpgrade.MAX_SIZE + 1) return false;
+
+        int magnetCount = 0;
+        Set<String> seenItems = new HashSet<>(MagnetUpgrade.MAX_SIZE);
+
+        for (ItemStack stack : input.getStacks()) {
+            if (stack.isEmpty()) continue;
+
+            Item item = stack.getItem();
+
+            if (item instanceof UpgradeItem) {
+                magnetCount++;
+                continue;
+            }
+
+            Identifier identifier = Registries.ITEM.getId(item);
+            if (hasCustomName(stack)) {
+                String tagId = parseTagNameFromStack(stack);
+                if (tagId == null || !seenItems.add(tagId)) return false;
+            } else if (!seenItems.add(identifier.toString())) return false;
+        }
+
+        int totalItems = seenItems.size();
+
+        return totalItems > 0
+                && totalItems <= MagnetUpgrade.MAX_SIZE
+                && magnetCount == 1;
+    }
+
+    @Override
+    public ItemStack craft(CraftingRecipeInput input, RegistryWrapper.WrapperLookup registries) {
+        ItemStack center = ItemStack.EMPTY;
+
+        ItemStack stack = findUpgradeStack(input);
+        if (stack.isEmpty()) return center;
+
+        UpgradeComponent oldComponent = stack.get(BackpackDataComponentTypes.UPGRADE);
+        if (oldComponent == null) return center;
+
+        List<String> uniqueItems = extractFilterKeysFromInput(stack, input);
+        if (uniqueItems.isEmpty()) return center;
+
+        return getResultStack(oldComponent, stack, uniqueItems);
+    }
+
+    public ItemStack getResultStack(UpgradeComponent oldComponent, ItemStack center, List<String> uniqueItems) {
+        Upgrade oldUpgrade = oldComponent.getUpgrade();
+        Upgrade upgrade = oldUpgrade.getType().create();
+        if (!(upgrade instanceof FilterableUpgrade newUpgrade)) return ItemStack.EMPTY;
+
+        ItemFilter filter = newUpgrade.getFilter();
+        filter.filterList().addAll(uniqueItems);
+        filter.setMode(((FilterableUpgrade) oldUpgrade).getFilter().getMode());
+
+        ItemStack result = center.copy();
+        result.set(BackpackDataComponentTypes.UPGRADE, UpgradeComponent.of((Upgrade) newUpgrade));
+
+        return result;
+    }
+
+    public ItemStack findUpgradeStack(CraftingRecipeInput input) {
+        for (ItemStack stack : input.getStacks()) {
+            if (stack.isEmpty()) continue;
+
+            UpgradeComponent component = stack.get(BackpackDataComponentTypes.UPGRADE);
+            if (component != null
+                    && component.getUpgrade() instanceof FilterableUpgrade
+            ) return stack;
+        }
+
+        return ItemStack.EMPTY;
+    }
+
+    public List<String> extractFilterKeysFromInput(ItemStack filterStack, CraftingRecipeInput input) {
+        Set<String> uniqueItems = new HashSet<>(MagnetUpgrade.MAX_SIZE);
+
+        for (ItemStack stack : input.getStacks()) {
+            if (uniqueItems.size() >= MagnetUpgrade.MAX_SIZE) break;
+            if (stack.isEmpty()) continue;
+            if (stack.isOf(filterStack.getItem())) continue;
+
+            String itemKey;
+
+            if (hasCustomName(stack)) {
+                itemKey = parseTagNameFromStack(stack);
+                if (itemKey == null) continue;
+            } else itemKey = Registries.ITEM.getId(stack.getItem()).toString();
+
+            uniqueItems.add(itemKey);
+        }
+
+        return new ArrayList<>(uniqueItems);
+    }
+
+    public String parseTagNameFromStack(ItemStack stack) {
+        String tagString = stack.getName().getString();
+        if (!tagString.startsWith("#")) return null;
+
+        Identifier tagId = Identifier.tryParse(tagString.substring(1));
+        if (tagId == null) return null;
+
+        TagKey<Item> tag = TagKey.of(RegistryKeys.ITEM, tagId);
+        var tagKey = Registries.ITEM.getOptional(tag);
+        if (tagKey.isEmpty()) return null;
+        if (!stack.isIn(tagKey.get())) return null;
+
+        return tagString;
+    }
+
+    private boolean hasCustomName(ItemStack stack) {
+        return stack.getCustomName() != null;
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getRecipeRemainders(CraftingRecipeInput input) {
+        DefaultedList<ItemStack> remainders = DefaultedList.ofSize(input.size(), ItemStack.EMPTY);
+
+        for (int i = 0; i < input.size(); i++) {
+            ItemStack stack = input.getStackInSlot(i);
+            if (stack.getItem() instanceof UpgradeItem) continue;
+
+            remainders.set(i, stack.copyAndEmpty());
+        }
+
+        return remainders;
+    }
+
+    @Override
+    public RecipeSerializer<InputDefinedFilterRecipe> getSerializer() {
+        return BackpackRecipeRegistry.MAGNET_FILTER_APPLIER_RECIPE;
+    }
+
+    @Override
+    public CraftingRecipeCategory getCategory() {
+        return CraftingRecipeCategory.MISC;
+    }
+
+    public static class Serializer extends SpecialRecipeSerializer<InputDefinedFilterRecipe> {
+        public Serializer(Factory<InputDefinedFilterRecipe> factory) {
+            super(factory);
+        }
+
+        @Override
+        public PacketCodec<RegistryByteBuf, InputDefinedFilterRecipe> packetCodec() {
+            return null;
+        }
+    }
+}

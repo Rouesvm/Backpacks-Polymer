390788af0fe54a5c72288fdafedf58bd9f423e3a
drivlo <rouesvmnowfree@gmail.com>
2025-07-18 12:31:13 +0800

    Try to implement the new data system in 30 minutes

---
+++ src/main/java/com/rouesvm/servback/technical/data/state/codecs/BackpackData.java
@@ -0,0 +1,43 @@
+package com.rouesvm.servback.technical.data.state.codecs;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.Uuids;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class BackpackData {
+    private final UUID uuid;
+    private final InventoryData itemStacks;
+
+    public BackpackData(UUID uuid, InventoryData inventory) {
+        this.uuid = uuid;
+        this.itemStacks = inventory;
+    }
+
+    public static final Codec<BackpackData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Uuids.INT_STREAM_CODEC.fieldOf("uuid").forGetter(BackpackData::getUuid),
+            InventoryData.CODEC.fieldOf("contents").forGetter(BackpackData::getInventoryData)
+    ).apply(instance, BackpackData::new));
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public InventoryData getInventoryData() {
+        return itemStacks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BackpackData that = (BackpackData) o;
+        return Objects.equals(uuid, that.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(uuid);
+    }
+}
---
+++ src/main/java/com/rouesvm/servback/technical/data/state/codecs/InventoryData.java
@@ -0,0 +1,30 @@
+package com.rouesvm.servback.technical.data.state.codecs;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+
+import java.util.List;
+
+public class InventoryData {
+    private final List<SlotData> itemStacks;
+
+    public InventoryData(List<SlotData> inventory) {
+        this.itemStacks = inventory;
+    }
+
+    public static final Codec<InventoryData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            SlotData.CODEC.listOf().fieldOf("Items").forGetter(InventoryData::getItemStacks)
+    ).apply(instance, InventoryData::new));
+
+    public List<SlotData> getItemStacks() {
+        return itemStacks;
+    }
+
+    public static DefaultedList<ItemStack> getHeldStacks(List<SlotData> data) {
+        DefaultedList<ItemStack> stacks = DefaultedList.ofSize(9 * 6, ItemStack.EMPTY);
+        if (!data.isEmpty()) data.forEach(slotData -> stacks.set(slotData.getSlot(), slotData.getStack()));
+        return stacks;
+    }
+}

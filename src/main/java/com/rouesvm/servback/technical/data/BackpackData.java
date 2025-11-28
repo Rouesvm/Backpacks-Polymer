c92f53194f90471a86a3c00bb7a9df2518a4a927
drivlo <rouesvmnowfree@gmail.com>
2025-11-28 23:11:32 +0800

    Only run DFU test if version is not the same.

--- src/main/java/com/rouesvm/servback/technical/data/BackpackData.java
+++ src/main/java/com/rouesvm/servback/technical/data/BackpackData.java
@@ -1,9 +1,7 @@
 package com.rouesvm.servback.technical.data;
 
-import com.mojang.datafixers.DataFixer;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.DataResult;
-import com.mojang.serialization.Dynamic;
 import com.rouesvm.servback.ServerBackpacks;
 import com.rouesvm.servback.technical.data.codecs.BackpackInstanceData;
 import com.rouesvm.servback.technical.data.codecs.InventoryData;
@@ -11,11 +9,9 @@ import com.rouesvm.servback.technical.manager.BackpackManager;
 import com.rouesvm.servback.technical.ui.inventory.BackpackInventory;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import net.minecraft.SharedConstants;
-import net.minecraft.datafixer.TypeReferences;
 import net.minecraft.nbt.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.WorldSavePath;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.DataInputStream;
 import java.io.IOException;
@@ -86,39 +82,6 @@ public class BackpackData {
         return false;
     }
 
-    private static void applyDataFixToItemStacks(@NotNull MinecraftServer server, NbtCompound root, int newVersion) {
-        DataFixer fixer = server.getDataFixer();
-
-        Optional<Integer> data_version = root.getInt("data_version");
-        if (data_version.isEmpty()) return;
-
-        Optional<NbtCompound> contents = root.getCompound("contents");
-        if (contents.isEmpty()) return;
-
-        Optional<NbtList> items = contents.get().getList("Items");
-        if (items.isEmpty()) return;
-
-        NbtList itemList = items.get();
-        for (int j = 0; j < itemList.size(); ++j) {
-            Optional<NbtCompound> slotCompound = itemList.getCompound(j);
-            if (slotCompound.isEmpty()) continue;
-
-            NbtCompound slot = slotCompound.get();
-
-            Optional<NbtCompound> wrapped = slot.getCompound("itemStacks");
-            if (wrapped.isEmpty()) continue;
-
-            Dynamic<NbtElement> inputDynamic = new Dynamic<>(NbtOps.INSTANCE, wrapped.get());
-            Dynamic<NbtElement> outputDynamic = fixer.update(
-                    TypeReferences.ITEM_STACK, inputDynamic,
-                    data_version.get(), newVersion
-            );
-
-            NbtCompound fixed = (NbtCompound) outputDynamic.getValue();
-            slot.put("itemStacks", fixed);
-        }
-    }
-
     private static Optional<BackpackInstance> loadSingle(Path saveDir, MinecraftServer server, UUID uuid) {
         if (saveDir == null) return Optional.empty();
 
@@ -127,7 +90,7 @@ public class BackpackData {
 
         try (DataInputStream dis = new DataInputStream(Files.newInputStream(file))) {
             NbtCompound nbt = NbtIo.readCompressed(dis, NbtSizeTracker.ofUnlimitedBytes());
-            applyDataFixToItemStacks(server, nbt, SharedConstants.getGameVersion().dataVersion().id());
+            BackpackDataDFU.applyDataFixToItemStacks(server, nbt, SharedConstants.getGameVersion().dataVersion().id());
 
             DataResult<Pair<BackpackInstanceData, NbtElement>> data =
                     BackpackInstanceData.CODEC.decode(server.getRegistryManager().getOps(NbtOps.INSTANCE), nbt);

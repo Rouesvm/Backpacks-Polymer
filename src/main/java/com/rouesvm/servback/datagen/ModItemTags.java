af94a863f42ac5986ea512f1022ee8ba3a35ffc8
drivlo <rouesvmnowfree@gmail.com>
2026-03-23 16:02:45 +0800

    Doesn't really make sense if it's not consistent

--- src/main/java/com/rouesvm/servback/datagen/ModItemTags.java
+++ src/main/java/com/rouesvm/servback/datagen/ModItemTags.java
@@ -54,7 +54,6 @@ public class ModItemTags extends FabricTagProvider.ItemTagProvider {
 
         this.getOrCreateRawBuilder(BACKPACKS).addOptionalElement(((BasicPolymerBlockItem) BackpackItemRegistry.GLOBAL_BACKPACK).getIdentifier());
         this.getOrCreateRawBuilder(BACKPACKS).addOptionalElement(((BasicPolymerBlockItem) BackpackItemRegistry.ENDER_BACKPACK).getIdentifier());
-
         this.getOrCreateRawBuilder(BACKPACKS).addOptionalElement(((BasicPolymerBlockItem) BackpackItemRegistry.LAVA_BACKPACK).getIdentifier());
 
         this.getOrCreateRawBuilder(BLACKLISTED).addOptionalElement(((BasicPolymerBlockItem) BackpackItemRegistry.ENDER_BACKPACK).getIdentifier());
